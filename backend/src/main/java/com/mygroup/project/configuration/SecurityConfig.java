ffa6a63fc40df8763e374e7f2041ce89e83b3cab
I-AzraeL-I <qaraqan70@gmail.com>
2020-11-29 12:33:01 +0100

    updated spring security config

--- backend/src/main/java/com/mygroup/project/configuration/SecurityConfig.java
+++ backend/src/main/java/com/mygroup/project/configuration/SecurityConfig.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserDetailsServiceImpl userDetailsService;
@@ -21,12 +23,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
-                //.authorizeRequests()
-                //.antMatchers("/logowanie").permitAll()
-                //.antMatchers("/rejestracja").permitAll()
-                //.anyRequest()
-                //.authenticated()
-                //.and()
+                .formLogin()
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/", true)
+                .and()
                 .httpBasic();
     }
 
