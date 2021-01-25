0d0b7e6e0a422acc7f16ccad6050f9320688263f
Durmix <Durmix@users.noreply.github.com>
2021-01-25 20:53:37 +0100

    Add opinion functionality

--- backend/src/main/java/com/mygroup/project/controller/ApplicationController.java
+++ backend/src/main/java/com/mygroup/project/controller/ApplicationController.java
@@ -1,26 +1,35 @@
 package com.mygroup.project.controller;
 
 import com.mygroup.project.model.dto.Roles;
+import com.mygroup.project.model.dto.basic.OpinionDTO;
 import com.mygroup.project.model.dto.basic.TutorDTO;
+import com.mygroup.project.model.dto.specialized.OpinionFormDTO;
 import com.mygroup.project.model.dto.specialized.UserSubjectOfferDTO;
+import com.mygroup.project.model.service.OpinionServiceImpl;
 import com.mygroup.project.model.service.TutorServiceImpl;
 import com.mygroup.project.model.service.UserSubjectServiceImpl;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Collection;
+import java.net.URI;
 
 @RestController
 public class ApplicationController {
 
     private final UserSubjectServiceImpl userSubjectService;
     private final TutorServiceImpl tutorService;
+    private final ModelMapper modelMapper;
+    private final OpinionServiceImpl opinionService;
 
-    public ApplicationController(UserSubjectServiceImpl userSubjectService, TutorServiceImpl tutorService) {
+    public ApplicationController(UserSubjectServiceImpl userSubjectService, TutorServiceImpl tutorService, ModelMapper modelMapper, OpinionServiceImpl opinionService) {
         this.userSubjectService = userSubjectService;
         this.tutorService = tutorService;
+        this.modelMapper = modelMapper;
+        this.opinionService = opinionService;
     }
 
     @GetMapping("/")
@@ -37,6 +46,13 @@ public class ApplicationController {
     public ResponseEntity<TutorDTO> getOffer(@PathVariable("id") Long tutorId) {
         TutorDTO tutorDTO = tutorService.get(tutorId);
         return ResponseEntity.ok(tutorDTO);
+    }
+
+    @PostMapping("/addOpinion")
+    public ResponseEntity<?> addOpinion(@Valid @RequestBody OpinionFormDTO opinionFormDTO) {
+        OpinionDTO opinionDTO = modelMapper.map(opinionFormDTO, OpinionDTO.class);
+        opinionDTO = opinionService.create(opinionDTO);
+        return ResponseEntity.created(URI.create("/opinion/" + opinionDTO.getOpinionId())).headers(new HttpHeaders()).body(opinionDTO.getOpinionId());
     }
 
 }
---
+++ backend/src/main/java/com/mygroup/project/model/dto/specialized/OpinionFormDTO.java
@@ -0,0 +1,28 @@
+package com.mygroup.project.model.dto.specialized;
+
+import com.mygroup.project.model.entity.Tutor;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+public class OpinionFormDTO {
+
+    @NotBlank
+    @Length(max = 255)
+    private String comment;
+
+    @NotNull
+    @Min(value = 0)
+    @Max(value = 5)
+    private Integer rating;
+
+    private Tutor tutor;
+
+}
