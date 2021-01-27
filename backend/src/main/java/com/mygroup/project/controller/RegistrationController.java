e889675485c42464607f72fed1c1d88deff6aa96
Durmix <Durmix@users.noreply.github.com>
2021-01-27 20:53:17 +0100

    add ScheduleDTO assignment

--- backend/src/main/java/com/mygroup/project/controller/RegistrationController.java
+++ backend/src/main/java/com/mygroup/project/controller/RegistrationController.java
@@ -2,8 +2,11 @@ package com.mygroup.project.controller;
 
 import com.mygroup.project.model.dto.basic.AddressDTO;
 import com.mygroup.project.model.dto.basic.ContactDTO;
+import com.mygroup.project.model.dto.basic.ScheduleDTO;
 import com.mygroup.project.model.dto.basic.UserDTO;
 import com.mygroup.project.model.dto.specialized.UserRegisterFormDTO;
+import com.mygroup.project.model.repository.ScheduleRepository;
+import com.mygroup.project.model.service.ScheduleServiceImpl;
 import com.mygroup.project.model.service.UserServiceImpl;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpHeaders;
@@ -17,10 +20,12 @@ import java.net.URI;
 public class RegistrationController {
 
     private final ModelMapper modelMapper;
+    private final ScheduleServiceImpl scheduleService;
     private final UserServiceImpl userService;
 
-    RegistrationController(ModelMapper modelMapper, UserServiceImpl userService) {
+    RegistrationController(ModelMapper modelMapper, ScheduleServiceImpl scheduleService, UserServiceImpl userService) {
         this.modelMapper = modelMapper;
+        this.scheduleService = scheduleService;
         this.userService = userService;
     }
 
@@ -28,9 +33,11 @@ public class RegistrationController {
     public ResponseEntity<?> registerUserAccount(@Valid @RequestBody UserRegisterFormDTO userRegisterFormDTO) {
         AddressDTO addressDTO = modelMapper.map(userRegisterFormDTO, AddressDTO.class);
         ContactDTO contactDTO = modelMapper.map(userRegisterFormDTO, ContactDTO.class);
+        ScheduleDTO scheduleDTO = modelMapper.map(userRegisterFormDTO, ScheduleDTO.class);
         UserDTO userDTO = modelMapper.map(userRegisterFormDTO, UserDTO.class);
         userDTO.setContact(contactDTO);
         userDTO.setAddress(addressDTO);
+        userDTO.setSchedule(scheduleDTO);
         userDTO = userService.create(userDTO);
         return ResponseEntity.created(URI.create("/profil/" + userDTO.getUserId())).headers(new HttpHeaders()).body(userDTO.getUserId());
     }
